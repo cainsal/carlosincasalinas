5a9146015ebc0ee742ac03f5892664c5be26a263
Carlos Inca Salinas <carlosalfre.incasalinas@gmail.com>
2023-08-23 08:27:11 -0500

    Commit first

---
+++ header/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- STYLES -->
+  <link rel="stylesheet" href="styles/css/stylesIndice.css">
+
+  <!-- FONTS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+  <!-- BOXICONS -->
+  <link rel="stylesheet" href="styles/boxicons/css/boxicons.min.css">
+
+  <!-- FONT AWESOME -->
+
+  
+ 
+  <link rel="stylesheet" href="styles/fontAwesome/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+
+
+</head>
+
+<body>
+  <header>    
+    <a href="#" class="logo"><img src="img/Logo_carlosincasalinas_01.png"></a>
+		<div class="bx bx-menu" id="menuIcon"></div>
+    <div> </div>
+    <ul class="navlist">
+      <li><a href="#"></a>Home</li>
+      <li><a href="#"></a>About</li>
+      <li><a href="#"></a>Portafolio</li>
+      <li><a href="#"></a>Service</li>
+      <li><a href="#"></a>contact</li>      
+    </ul>
+    <a href="#" class="top-btn">Download CV</a>
+  </header>
---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	var typed = new Typed(".typing-1",{
+		strings: [
+			"Carlos Inca","Developer", "Designer", "TI", "Freelancer"
+		],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	});
+
+});
